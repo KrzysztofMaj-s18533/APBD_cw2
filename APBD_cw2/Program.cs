41e786105d3d5e6fc7c66dbb5a2af7637b21b512
Krzysztof Maj <s18533@pjwstk.edu.pl>
2020-03-09 11:05:13 +0100

    Commiting initial variables and setup

---
+++ APBD_cw2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace APBD_cw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //default path variables for data import and export + data format
+            var dPathToCsv = @"C:\Users\s18533\Desktop\dane.csv";
+            var dPathToXml = @"C:\Users\s18533\Desktop\result.xml";
+            var dFormat = "xml";
+
+            var logPath = @"C:\Users\s18533\Desktop\log.txt";   //log, used for errors (ArgumentException()/FileNotFoundException())
+
+            var lines = File.ReadLines(dPathToCsv);
+            
+
+            foreach(var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            var today = DateTime.UtcNow;
+
+            Console.WriteLine(today);
+
+            //var hash = new HashSet<Student>(new )
+        }
+    }
+}
